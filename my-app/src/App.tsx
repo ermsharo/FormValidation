f835b1d2cc0bda78315e7e57d42f420f1d851476
Emilio Haro <ermsharo@gmail.com>
2023-01-30 14:08:32 -0300

    update changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,81 +1,10 @@
-import * as yup from "yup";
-import { useFormik } from "formik";
-import TextField from '@mui/material/TextField';
+import Form  from "./components/Form/index";
+
 function App() {
-
-  let schema = yup.object().shape({
-    nome: yup.string().required(),
-    email: yup.string().email().required(),
-    idade: yup.number().required().positive().integer(),
-    website: yup.string().url()
-  });
-
-
-    const formik = useFormik({
-      initialValues: {
-        nome: "",
-        email: "",
-        idade: "",
-      },
-      validationSchema: yup.object({
-        nome: yup.string().required("O campo é obrigatório."),
-        email: yup
-          .string()
-          .email("E-mail inválido.")
-          .required("O campo é obrigatório."),
-        idade: yup
-          .number()
-          .required("O campo é obrigatório.")
-          .positive("O campo deve ser positivo.")
-          .integer("O campo deve ser um número inteiro."),
-      }),
-      onSubmit: (values) => {
-        alert(JSON.stringify(values, null, 2));
-      },
-    });
 
   return (
     <div className="App">
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="nome">Nome</label>
-      <TextField
-      label = "nome"
-        id="nome"
-        name="nome"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.nome}
-      />
-      {formik.touched.nome && formik.errors.nome ? (
-        <div>{formik.errors.nome}</div>
-      ) : null}
-      <label htmlFor="email">E-mail</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
-      />
-      {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
-      ) : null}
-      <label htmlFor="idade">Idade</label>
-      <input
-        id="idade"
-        name="idade"
-        type="number"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.idade}
-      />
-      {formik.touched.idade && formik.errors.idade ? (
-        <div>{formik.errors.idade}</div>
-      ) : null}
-      <button type="submit">Enviar</button>
-    </form>
+    <Form/>
     </div>
   );
 }
---
+++ my-app/src/components/Form/index.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { Typography } from "@mui/material";
+
+interface Props {}
+
+
+const Form = (props: Props) => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      telephone: "",
+      email: "",
+      date: "",
+    },
+    validationSchema: yup.object({
+      name: yup
+      .string()
+      .required("O campo é obrigatório.")
+      .matches(/^[a-z]+$/ , 'Nomes não podem conter números')
+      ,
+      telephone: yup
+      .number()
+      .required("O campo é obrigatório.")
+      .integer("O campo deve conter apenas numeros"),
+
+      
+      email: yup
+        .string()
+        .email("E-mail inválido.")
+        .required("O campo é obrigatório."),
+      date: yup
+        .date()
+        .required("O campo é obrigatório.")
+
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+
+    <Container maxWidth="sm">
+
+
+    <form>
+
+<Stack sx={{ width: '100%' }} spacing={2}>
+<Typography variant="h2" >
+  Cadastro
+</Typography>
+   <TextField
+    label="nome"
+    id="name"
+    name="name"
+    type="text"
+    onChange={formik.handleChange}
+    onBlur={formik.handleBlur}
+    value={formik.values.name}
+  />
+  {formik.touched.name && formik.errors.name ? (
+     <Alert severity="error">{formik.errors.name}</Alert>
+
+  ) : null}
+  <TextField
+    label="telefone"
+    id="telephone"
+    name="telephone"
+    type="text"
+    onChange={formik.handleChange}
+    onBlur={formik.handleBlur}
+    value={formik.values.telephone}
+  />
+  {formik.touched.telephone && formik.errors.telephone ? (
+
+    <Alert severity="error">{formik.errors.telephone}</Alert>
+  ) : null}
+
+  <TextField
+    label="email"
+    id="email"
+    name="email"
+    type="text"
+    onChange={formik.handleChange}
+    onBlur={formik.handleBlur}
+    value={formik.values.email}
+  />
+  {formik.touched.email && formik.errors.email ? (
+    <Alert severity="error">{formik.errors.email}</Alert>
+
+
+  ) : null}
+
+  <TextField
+    label="data"
+    id="date"
+    name="date"
+    type="text"
+    onChange={formik.handleChange}
+    onBlur={formik.handleBlur}
+    value={formik.values.date}
+  />
+  {formik.touched.date && formik.errors.date ? (
+    <Alert severity="error">{formik.errors.date}</Alert>
+  ) : null}
+
+  <Button type="submit" variant="contained">Enviar</Button>
+  
+
+ 
+</Stack>
+
+ 
+</form>
+  </Container>
+
+   
+  );
+};
+
+export default Form;
